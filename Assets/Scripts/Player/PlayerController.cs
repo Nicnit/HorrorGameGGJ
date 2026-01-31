4ac4de1a7a116b50aeedb18dd1b40537dde70a91
KJ <kjk@skyhopper.net>
2026-01-31 14:26:23 -0500

    finished playercontroller, added rough sprint and stamina, fixed stunlock and mouse look
    stunlock may not be working as expected, but it was blocking movement before; also, planning on revising the stamina system.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
     public float jumpForce = 5f;
-    public float sprintSpeed = 5f;
+    public float sprintSpeed = 10f;
     public bool canMove = true;
 
     [Header("Ground")]
@@ -18,15 +18,23 @@ public class PlayerController : MonoBehaviour
     private Vector2 inputDir;
     private bool shouldJump;
 
+    private bool isSprinting;
+    private float stamina = 0f;
+    private float maxStamina = 5f;
+    [SerializeField] float degenRate = 3f;
+    [SerializeField] float regenRate = 0.5f;
+
     private Rigidbody rb;
     private InputAction moveAction;
     private InputAction jumpAction;
+    private InputAction sprintAction;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        sprintAction = InputSystem.actions.FindAction("Sprint");
     }
 
     private void Update()
@@ -35,12 +43,13 @@ public class PlayerController : MonoBehaviour
         // Handle player being stunned (Critical nonmovement logics should be before this)
         bool stunned = stunTime < maxStunTime;
         if (stunned)
+        {
             // Can't move, jump, etc.
-            return;
-        else
             stunTime += Time.deltaTime;
-        
-        
+            return;
+        }
+
+
         inputDir = moveAction.ReadValue<Vector2>();
 
         UpdateGround();
@@ -48,6 +57,14 @@ public class PlayerController : MonoBehaviour
         if (jumpAction.WasPerformedThisFrame() && isGrounded)
         {
             shouldJump = true;
+        }
+        else if (sprintAction.WasPressedThisFrame())
+        {
+            isSprinting = true;
+        }
+        else if (sprintAction.WasReleasedThisFrame())
+        {
+            isSprinting = false;
         }
     }
 
@@ -70,11 +87,28 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        if (!canMove) return;
+        if (!canMove || !isGrounded) return;
         
         Vector3 moveDir = transform.right * inputDir.x + transform.forward * inputDir.y;
-        Vector3 velocity = moveDir * walkSpeed;
+        Vector3 velocity;
+        if (isSprinting)
+        {
+            velocity = moveDir * sprintSpeed;
+            stamina = Mathf.Max(0f, stamina - Time.fixedDeltaTime * degenRate);
+            if (stamina <= 0f)
+            {
+                isSprinting = false;
+            }
+        } else 
+        { 
+            velocity = moveDir * walkSpeed;
+            if (stamina < maxStamina)
+            {
+                stamina = Mathf.Clamp(stamina + regenRate * Time.fixedDeltaTime, 0f, maxStamina);
+            }
+        }
         rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+        Debug.Log(stamina + ", " + isSprinting);
     }
 
     void UpdateGround()
@@ -83,7 +117,7 @@ public class PlayerController : MonoBehaviour
     }
 
     private float maxStunTime = 3f;
-    private float stunTime = 0;
+    private float stunTime = 3f;
     public void StunLock(float newStunTime)
     {
         float stunTimeLeft = maxStunTime - stunTime;
