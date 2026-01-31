3fbda4c242fb781725c711235c381d7187ee17f6
KJ <kjk@skyhopper.net>
2026-01-31 17:46:10 -0500

    fixed camera jitter

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -4,48 +4,38 @@ public class MouseLook : MonoBehaviour
 {
     public float minPitch = -90f;
     public float maxPitch = 90f;
-    public float sensitivity = 100f;
+    public float sensitivity = 0.08f;
 
-    public Rigidbody playerRb;
+    // public Transform neck;
+    public Transform playerBody;
 
     private float pitch = 0f;
-    private float yaw = 0f;
 
     private InputAction lookAction;
 
     private void Start()
     {
         LockCursor(true);
-        playerRb = transform.parent.GetComponent<Rigidbody>();
         lookAction = InputSystem.actions.FindAction("Look");
-
-        yaw = playerRb.rotation.eulerAngles.y;
     }
 
-    // Update is called once per frame
-    void Update()
+  
+    void LateUpdate()
     {
         Vector2 rawInput = lookAction.ReadValue<Vector2>();
-        
-        Vector2 targetDir =  rawInput * sensitivity * Time.deltaTime; // could optionally smooth here instead
+        Vector2 targetDir =  rawInput * sensitivity; // could optionally smooth here instead
 
         pitch -= targetDir.y;
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
 
         // apply rotations
+        playerBody.Rotate(Vector3.up * targetDir.x);
         transform.localRotation = Quaternion.Euler(pitch, 0f, 0f);
-        yaw += targetDir.x;
-    }
-
-    private void FixedUpdate()
-    {
-        Quaternion target = Quaternion.Euler(0f, yaw, 0f);
-        playerRb.MoveRotation(target);
     }
 
     void LockCursor(bool shouldLock) 
     { 
         Cursor.lockState = shouldLock ? CursorLockMode.Locked : CursorLockMode.None;
-        Cursor.visible = shouldLock;
+        Cursor.visible = !shouldLock;
     }
 }
