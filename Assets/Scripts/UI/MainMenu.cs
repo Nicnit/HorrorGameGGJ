ab80fa0c3d81908fdcc338b3b45d1ed4835c5d9b
huedeane <hiiamaalien@gmail.com>
2026-02-01 12:10:31 -0500

    Fix Bug

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameTest");
+        SceneManager.LoadScene("Level Generation");
         Time.timeScale = 1f;
     }
 
